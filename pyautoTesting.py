b3a844b4bd6b49664000295f35cd8e9cf5976b34
jakobliggett <jakobliggett@gmail.com>
2017-12-08 15:49:00 -0500

    Testing 1

--- pyautoTesting.py
+++ pyautoTesting.py
@@ -1,1 +1,10 @@
+##Supreme Bot 0.01
 import pyautogui
+import os
+
+pyautogui.PAUSE = 1 ##Seconds to wait between actions
+lerp = 0.5
+
+width, height = pyautogui.size()
+
+os.system('open http://www.google.com')
