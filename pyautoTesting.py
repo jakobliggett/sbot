90797759a58e7b51fc115e8c3e53ff8e9378b16b
jakobliggett <jakobliggett@gmail.com>
2017-12-08 16:10:03 -0500

    Testing with parsing google results

--- pyautoTesting.py
+++ pyautoTesting.py
@@ -1,3 +1,12 @@
 import webbrowser
-import os
-webbrowser.open('http://www.google.com')
+import requests, sys, webbrowser, bs4, os
+
+#webbrowser.open('http://www.google.com')
+
+res = requests.get('http://google.com/search?q=' + 'eddie')
+res.raise_for_status()
+
+soup = bs4.BeautifulSoup(res.text, "html5lib")
+linkElems = soup.select('.r a')
+for i in range(len(linkElems)):
+    webbrowser.open('http://google.com' + linkElems[i].get('href'))
