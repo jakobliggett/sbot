ef5da9261a6e92f1b6d54d6cbf91cba0f4e4ba06
EddieCeausu <eddieswim101@gmail.com>
2017-12-09 17:55:09 -0500

    finished get_checkoutdata; started checkout function

--- checkoutTesting.py
+++ checkoutTesting.py
@@ -1,30 +1,24 @@
-#from selenium import webdriver
-#from selenium.webdriver.common.keys import Keys
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import os
 
-#browser = webdriver.Chrome("{}/chromedriver".format(current_direc))
-'''
-Alternative 1 with each value having a variable
-with open("checkoutdata.txt") as inp:
-    data = inp.read().splitlines()
-print(data)
-name = data[0].split(': ', 1)[-1]
-print(name)
-email = data[1].split(': ', 1)[-1]
-tel = data[2].split('')
-'''
-d = {}
-nkey = ""
-with open("checkoutdata.txt") as f:
-    for line in f:
-        key, val = line.split(": ")
-        if key.find("\n") != -1:
-            key = key.replace("\n", "")
-        if val.find("\n") != -1:
-            val = val.replace("\n", "")
-        d[key] = val
-print(d)
-for key, val in d.items():
-    if val == "N/A":
-        del d[key]
-print(d)
-#namef = browser.find_element_by_class_name("payment")
+current_direc = os.path.dirname(os.path.abspath(__file__))
+browser = webdriver.Chrome()
+
+#Alternative 1 with each value having a variable
+def get_checkoutdata(in_file):
+    with open(in_file) as inp:
+        data = inp.read().splitlines()
+    dic = {}
+    #print(data)
+    for i in data:
+        dic[i.split(':')[0]] = i.split(': ', 1)[-1]
+    print(dic)
+    return dic
+
+def checkout(data):
+    browser.get('http://www.supremenewyork.com/checkout')
+    namef = browser.find_element_by_class_name("payment")
+    
+if __name__ == '__main__':
+    checkout(get_checkoutdata("checkoutdata.txt"))
