edf4ace7d128b12d0fb2750c21d799b55fee8dc5
jakobliggett <jakobliggett@gmail.com>
2017-12-13 22:26:04 -0500

    Faster checkout working and fixed zip code/city glitch

--- AntiBotMisc.py
+++ AntiBotMisc.py
@@ -1,9 +1,9 @@
 import random, time
 import selenium
 
-def send(object, mesg, wpm=80, random_fuzzing=0.1):
+def send(object, mesg, wpm=700, random_fuzzing=0.1):
     cpm = wpm * 5
-    time_per_char = 60/cpm
+    time_per_char = float(60)/float(cpm)
     for char in mesg:
         time.sleep(time_per_char)
         object.send_keys(char)
--- BetaBot.py
+++ BetaBot.py
@@ -22,7 +22,7 @@ def LoadConfiguration(cfile):
     print(config)
     return config
 
-def FindProducts(browser, site):
+def FindProducts(browser, site, config):
     browser.get(site)
     unsorted_products = browser.find_elements_by_xpath("""//*[@id="container"]/article[*]/div/a""")
     #"""//*[@id="container"]/article[88]/div/a"""
@@ -35,7 +35,12 @@ def FindProducts(browser, site):
         except Exception: ##I put this in here so it only triggers if
             category = (href_split[4])
             link = ("{}/{}/{}".format(site, href_split[5], href_split[6]))
-            products.append([category, link])
+            if config[category] > 0: ##Only add to check list if we're looking for them
+                products.append([category, link])
+    if len(products) == 0:
+        print('No products available!')
+        time.sleep(1) ##Refresh Delay
+        return 0
     return products
 
 
@@ -48,7 +53,7 @@ def ProductsToCart(browser, products, config):
         try:
             product_name = browser.find_element_by_class_name("protect").text
             product_price = float((browser.find_element_by_class_name("price").text[1:]).replace(',', '')) ##Strips dollar sign and converts to int
-            print(product_name, product_price)
+            #print(product_name, product_price)
         except:
             product_name = 'N/A'
             product_price = 0
@@ -60,10 +65,11 @@ def ProductsToCart(browser, products, config):
                 print('\nBuying {}, current cart:{}, cart after this: {}'.format(product_name, current_cart_value, current_cart_value+product_price))
                 current_cart_value += product_price
             if (config['max_cart']-current_cart_value) <= config['max_cart']*0.2: ##if cart is within 20% of max checkout early
+                print('Break early, cart {}, difference {}, max cart {}'.format(current_cart_value, config['max_cart']-current_cart_value, config['max_cart']))
                 break ##Cart full, maxed on price
         except:
             pass
-        time.sleep(0.2) ##Is this necessary? # No
+        time.sleep(0.1) ##Is this necessary? # No
         browser.back()
     print('CART FULL!')
 
@@ -80,37 +86,57 @@ def get_checkoutdata(in_file):
 def Checkout(browser):
     checkout_button = browser.find_element_by_css_selector(".button.checkout")
     checkout_button.click()
+    ##Add E's code in here later
     data = get_checkoutdata('checkoutdata.txt')
     for did in data:
-        print(data[did])
-        if did == 'order_billing_state' or 'credit_card_month' or 'credit_card_year':
-            option = browser.find_element_by_id(did)
-            option.click()
-            option.send_keys(data[did])
-            option.send_keys(Keys.ENTER)
-        else:
-            element = browser.find_element_by_id(did)
-            element.click()
-            AntiBotMisc.send(element, data[did])
+        if not did == "order_billing_city": ##Lazy solution but oh well
+            if did == 'order_billing_state' or 'credit_card_month' or 'credit_card_year':
+                option = browser.find_element_by_id(did)
+                option.click()
+                #option.send_keys(data[did])
+                AntiBotMisc.send(option, data[did])
+                option.send_keys(Keys.ENTER)
+            else:
+                if did == 'N/A':
+                    pass
+                else:
+                    element = browser.find_element_by_id(did)
+                    element.click()
+                    time.sleep(0.1)
+                    """
+                    element.send_keys(Keys.CONTROL + 'a')
+                    element.send_keys(Keys.DELETE)
+                    element.send_keys(Keys.COMMAND + 'a') ##Mac Version
+                    element.send_keys(Keys.DELETE)
+                    """
+                    #element.send_keys(data[did])
+                    AntiBotMisc.send(element, data[did])
 
-    element = browser.find_element_by_id('order_billing_city')
-    element.click()
-    element.clear()
-    element.send_keys(data['order_billing_city'])
-    browser.find_element_by_css_selector('input#order_terms.checkbox').click()
-    time.sleep(3.5)
-    ppay = browser.find_element_by_css_selector('input.button')
-    ppay.click()
-
+    time.sleep(1)
+    print("\nORDERING!")
+    try: ##Captcha makes this fail, switch to manual mode
+        terms = browser.find_element_by_id("order_terms")
+        terms.click()
+        time.sleep(2)
+        #checkout_final = browser.find_element_by_name("commit")
+        #checkout_final.click()
+    except Exception as e:
+        print("MANUAL MODE ENGAGED!\n {}\n MANUAL MODE ENGAGED!".format(str(e)))
 
 def main():
     #site = "http://webcache.googleusercontent.com/search?q=cache:http://www.supremenewyork.com/shop/all"
     site = "http://www.supremenewyork.com/shop/all"
-    chromedriver_path = "{}/chromedriver".format( os.path.dirname(os.path.abspath(__file__)) )
-    browser = webdriver.Chrome(chromedriver_path)
-    products = FindProducts(browser, site)
-    print(products)
+    current_path = "{}/chromedriver".format( os.path.dirname(os.path.abspath(__file__)) )
     config = LoadConfiguration('prices_config.txt') ##Change this?
+    browser = webdriver.Chrome(current_path)
+    products = 0
+    while products == 0: ##Loops until something found not sold out
+        products = FindProducts(browser, site, config)
+    try:
+        os.system("afplay {}/Alarm.mp3".format(current_path))
+    except:
+        pass ##This will prob fail on windows
+    print(products)
     ProductsToCart(browser, products, config)
     Checkout(browser)
     time.sleep(20) ##Just for debug so chrome doesn't eat all the ram
